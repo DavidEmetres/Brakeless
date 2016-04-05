b3451551986d3ea845385666a72e78e89ba29b9a
DavidMartinez95 <david.martinez.martin.1995@gmail.com>
2016-04-05 10:50:11 +0200

    Commit Event Handler v1.1
    Added respawn and respawn boost

--- Assets/Project Assets/Scripts/CarEventHandler.cs
+++ Assets/Project Assets/Scripts/CarEventHandler.cs
@@ -7,13 +7,32 @@ public class CarEventHandler : MonoBehaviour {
 	private Transform respawnPos;
 	private Transform nextCheckPoint;
 	private Rigidbody carRigidbody;
+	private bool boost = false;
+	private float timer = 0f;
+	private bool hasBeenTriggered = false;
+	
+	public float boostForce = 50000f;
+	public float boostTime = 5f;
 
 	void Start () {
 		carRigidbody = GetComponent<Rigidbody>();
 	}
 	
 	void FixedUpdate () {
-		
+		if(boost)
+		{
+			if(timer >= 0)
+			{
+				Boost();
+				timer -= Time.deltaTime;
+			}
+			
+			else 
+			{
+				boost = false;
+				Debug.Log("Boost Desactivated");
+			}
+		}
 	}
 	
 	void OnTriggerEnter(Collider other)
@@ -27,6 +46,17 @@ public class CarEventHandler : MonoBehaviour {
 		{
 			Recolocate();
 		}
+		
+		if(other.tag == "Floor")
+		{
+			if(!hasBeenTriggered)
+			{
+				hasBeenTriggered = true;
+				timer = Time.deltaTime + boostTime;
+				boost = true;
+				Debug.Log("Boost Activated");
+			}
+		}
 	}
 	
 	void OnTriggerStay(Collider other)
@@ -39,6 +69,8 @@ public class CarEventHandler : MonoBehaviour {
 	
 	void Recolocate() 
 	{
+		hasBeenTriggered = false;
+		
 		respawnPos = GameObject.Find(checkPoint).transform;
 		
 		int temp = int.Parse(checkPoint) + 1;
@@ -46,7 +78,7 @@ public class CarEventHandler : MonoBehaviour {
 		
 		nextCheckPoint = GameObject.Find(stemp).transform;
 		
-		transform.position = new Vector3(respawnPos.position.x, respawnPos.position.y + 5f, respawnPos.position.z);
+		transform.position = new Vector3(respawnPos.position.x, respawnPos.position.y + 3f, respawnPos.position.z);
 		
 		Vector3 v = nextCheckPoint.position - transform.position;
 		Quaternion q = Quaternion.LookRotation(v);
@@ -56,5 +88,10 @@ public class CarEventHandler : MonoBehaviour {
 		carRigidbody.constraints = RigidbodyConstraints.FreezeRotationX;
 		carRigidbody.constraints = RigidbodyConstraints.FreezeRotationZ;
 		carRigidbody.constraints = RigidbodyConstraints.FreezeRotationY;
+	}
+	
+	void Boost()
+	{
+		carRigidbody.AddForce(new Vector3(0, 0, boostForce * Time.deltaTime), ForceMode.Force);
 	}
 }
